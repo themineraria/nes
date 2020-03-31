599c9d364efaec170569cc7efdf3dd49bdea43ba
Cyprien <cyp.isnard@hotmail.fr>
2020-04-01 01:55:43 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  console.log('Logged in as ${client.user.tag}!');
+  console.log('Logged in as ' + client.user.tag + '!');
 });
 
 client.on('message', msg => {
@@ -15,10 +15,11 @@ client.on('message', msg => {
     // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
     // To get the "message" itself we join the `args` back into a string with spaces: 
     const sayMessage = msg.content.split(" ");
+    sayMessage.splice(0, 1);
     // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
     msg.delete().catch(O_o=>{}); 
     // And we get the bot to say the thing: 
-    msg.channel.send(sayMessage[1]);
+    msg.channel.send(sayMessage.join(" "));
   }
   
 });
