24efdad1966fcc9a38391fa5d86228efc48cc302
Cyprien <cyp.isnard@hotmail.fr>
2020-04-01 02:06:11 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,18 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+var express = require('express')
+var app = express()
+
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function(request, response) {
+  response.send('Hello World!')
+})
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
 
 client.on('ready', () => {
   console.log('Logged in as ' + client.user.tag + '!');
